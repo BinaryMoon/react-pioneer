63fdb097b4a6ed3fab703bb04b44e3e507638557
BinaryMoon <binarymoon@gmail.com>
2018-11-25 09:22:34 +0000

    Remove App from Router and add Home scaffolding

---
+++ src/screens/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../components/Header";
+
+class Home extends React.Component {
+
+	render() {
+
+		return (
+			<React.Fragment>
+				<Header></Header>
+			</React.Fragment>
+		);
+
+	};
+
+};
+
+export default Home;
